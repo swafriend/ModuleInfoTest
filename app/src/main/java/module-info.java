4ca0550921ce680b73fe10b2ae962b24d594f2c2
swafriend <RXR05747@nifty.com>
2023-06-24 23:54:40 +0900

    module-info.java comment add

--- app/src/main/java/module-info.java
+++ app/src/main/java/module-info.java
@@ -1,5 +1,7 @@
 module gradle.multi.project.app.main {
+    // implementation project(':domain')
     requires gradle.multi.project.domain.main;
     requires spring.boot.autoconfigure;
     requires spring.boot;
 }
+// java　右クリック　新規　module-info.java
